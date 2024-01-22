cf107b99718b80b8514b613deb80432e282df583
Trương Thành Ä?ại <thanhdaibl1572002@gmail.com>
2024-01-22 21:20:37 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import ChatBox from '@/components/layouts/ChatBox'
 import { useSession } from 'next-auth/react'
 import db from '@/firebase/db'
 import { ref, get, set } from 'firebase/database'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const Home: FC = ({ }) => {
   const { data: session } = useSession({ required: true })
@@ -42,7 +45,7 @@ const Home: FC = ({ }) => {
 
 
   return (
-    <main className={styles._container}>
+    <main className={`${styles._container} ${inter.className}`}>
       <ChatBox />
     </main>
   )
