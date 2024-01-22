a2d633520dd9be2f00fad9d436e46faaeb01d885
Trương Thành Ä?ại <thanhdaibl1572002@gmail.com>
2024-01-22 21:04:19 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ const Home: FC = ({ }) => {
 
   useEffect(() => {
     if (session && session.user.id !== authId) {
-      window.location.reload()
       setAuthId(session.user.id)
       localStorage.setItem('DAIBL_userId', session.user.id)
       const messageId = Date.now().toString()
